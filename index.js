1fbc198f202f97b09f0b71bc57e6d6a914904119
ihollander <ianholla@gmail.com>
2018-11-18 17:43:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) { 
+    return driver.toUpperCase() === string.toUpperCase() 
+  }); 
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(driver) {
+    return driver.substring(0,string.length) === string;
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(driver) {
+    return driver.name === name;
+  });
+}
